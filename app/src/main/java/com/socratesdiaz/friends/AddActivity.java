6ff61fa323b8646eb4052a1b4078dedb280f14e8
Sócrates Díaz <sdsxd1800@gmail.com>
2016-10-17 23:29:54 -0400

    Adding AddActivity

---
+++ app/src/main/java/com/socratesdiaz/friends/AddActivity.java
@@ -0,0 +1,9 @@
+package com.socratesdiaz.friends;
+
+/**
+ * Created by Sócrates Díaz S on 10/17/2016.
+ */
+
+public class AddActivity {
+
+}
--- app/src/main/java/com/socratesdiaz/friends/MainActivity.java
+++ app/src/main/java/com/socratesdiaz/friends/MainActivity.java
@@ -1,13 +1,54 @@
 package com.socratesdiaz.friends;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if(fragmentManager.findFragmentById(android.R.id.content) == null) {
+            FriendsListFragment friendsListFragment = new FriendsListFragment();
+            fragmentManager.beginTransaction().add(android.R.id.content, friendsListFragment).commit();
+        }
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addRecord:
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startIntentSender(intent);
+                break;
+            case R.id.deleteDatabase:
+                FriendsDialog dialog = new FriendsDialog();
+                Bundle args = new Bundle();
+                args.putString(FriendsDialog.DIALOG_TYPE, FriendsDialog.DELETE_DATABASE);
+                dialog.setArguments(args);
+                dialog.show(getSupportFragmentManager(), "delete-database");
+                break;
+            case R.id.search:
+                Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(searchIntent);
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
